0dceec5f1102067601768bf096eedf332167add2
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-06-22 13:03:21 +0000

    add inlang.config.js and fix broken JSON

---
+++ frontend/inlang.config.js
@@ -0,0 +1,16 @@
+/**
+  * @type { import("@inlang/core/config").DefineConfig }
+  */
+export async function defineConfig(env) {
+  const { default: i18nextPlugin } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js');
+
+  const { default: standardLintRules } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js');
+
+  return {
+    referenceLanguage: 'en',
+    plugins: [
+      i18nextPlugin({ pathPattern: 'assets/translations/{language}.json', ignore: ["languages.json"] }),
+      standardLintRules(),
+    ],
+  };
+}
